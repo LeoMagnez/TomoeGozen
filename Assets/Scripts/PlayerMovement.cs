15731fcdc108129eef4eb1b254cd04f5d24ce0bf
Lothrinn <93184555+LeoMagnez@users.noreply.github.com>
2023-06-21 02:14:10 +0200

    Mouvement de base du controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 5f;
+
+    public PlayerInputActions playerControls;
+
+    Vector3 moveDirection = Vector3.zero;
+    private InputAction move;
+    private InputAction fire;
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        move = playerControls.Player.Move;
+        move.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = move.ReadValue<Vector3>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveDirection.x * moveSpeed,0f, moveDirection.z * moveSpeed);
+    }
+}
