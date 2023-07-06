e73d24a96f0211e234e915dcab626688863486e2
Lothrinn <93184555+LeoMagnez@users.noreply.github.com>
2023-07-06 23:21:39 +0200

    camera 45deg

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,6 +14,9 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float playerSpeed = 2.0f;
+
+    [SerializeField]
+    private float rotationSpeed = 2f;
     [SerializeField]
     private float jumpHeight = 1.0f;
     [SerializeField]
@@ -23,29 +26,21 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 playerVelocity;
     private bool groundedPlayer;
 
-    [Header("Camera")]
-    [SerializeField]
-    private InputActionReference lookControls;
-    [SerializeField]
-    private float rotationSpeed = 4f;
-    private Transform cameraTransform;
 
     private void OnEnable()
     {
         movementControls.action.Enable();
-        lookControls.action.Enable();
     }
 
     private void OnDisable()
     {
         movementControls.action.Disable();
-        lookControls.action.Disable();
     }
 
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
-        cameraTransform = Camera.main.transform;
+
     }
 
     void Update()
@@ -58,8 +53,6 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 movement = movementControls.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(movement.x, 0, movement.y);
-        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
-        move.y = 0f;
 
         controller.Move(move * Time.deltaTime * playerSpeed);
 
@@ -70,7 +63,7 @@ public class PlayerMovement : MonoBehaviour
 
         if(movement != Vector2.zero)
         {
-            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg;
             Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
         }
